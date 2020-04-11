fa196bea18d5be06cbf17538bc0e05f02850cfc9
Roberto Rolo <robertorolo@gmail.com>
2020-04-11 17:25:00 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 echo "This script will install arch linux."
 
 # keyboard configuration
-keyboard_layout = us
+keyboard_layout=us
 echo "Configuring keyboard as: $keyboard_layout"
 loadkeys $keyboard_layout
