88f73c8dda791e98bf328b88f9c95f2dd8126f00
Roberto Rolo <robertorolo@gmail.com>
2020-04-11 17:32:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,11 @@ echo "This script will install arch linux."
 keyboard_layout=us
 echo "Configuring keyboard as: $keyboard_layout"
 loadkeys $keyboard_layout
+
+# defining if BIOS or UEFI
+if [[ -d /sys/firmware/efi/efivars ]]; then
+  echo "UEFI system"
+else
+  echo "BIOS system"
+fi
+  
