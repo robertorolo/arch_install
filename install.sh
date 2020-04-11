b4e5bc9eae6de510b4768d1b0ef447e6c1a2f1fe
Roberto Rolo <robertorolo@gmail.com>
2020-04-11 17:42:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 echo "This script will install arch linux."
+sleep 3
 
 # Set the keyboard layout
 keyboard_layout=us
 echo "Setting keyboard to: $keyboard_layout"
 loadkeys $keyboard_layout
+sleep 3
 
 # Update the system clock
 timedatectl set-ntp true
 timedatectl status
+sleep 3
+
+# Partition the disks
+fdisk -l
+read -p "Type sdx > " DISK
+echo "arch will be installed in $DISK"
+sleep 3
 
 # Verify the boot mode
 if [[ -d /sys/firmware/efi/efivars ]]; then
@@ -17,8 +26,3 @@ if [[ -d /sys/firmware/efi/efivars ]]; then
 else
   echo "Boot mode BIOS"
 fi
-
-# Partition the disks
-fdisk -l
-read -p "Type sdx > " DISK
-echo "arch will be installed in $DISK"
