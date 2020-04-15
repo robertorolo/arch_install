84e3e71e314389bc357c854ec94986402ef6588c
Roberto Rolo <robertorolo@gmail.com>
2020-04-15 20:36:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -107,7 +107,7 @@ fi
 
 # Install essential packages
 echo "Instaling essential packages."
-pacstrap /mnt base linux linux-firmware git vim man-db man-pages texinfo networkmanager sudo
+pacstrap /mnt base linux linux-firmware git vim man-db man-pages texinfo networkmanager sudo curl
 
 # Generate an fstab file
 echo "Generating fstab file."
