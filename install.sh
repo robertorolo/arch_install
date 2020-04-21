c68053d75251f4724e5d092d7c474343fcb50e2c
Roberto Rolo <robertorolo@gmail.com>
2020-04-21 12:49:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "This script will install arch linux."
 sleep 1
 
 # Set the keyboard layout
-read -p "What is the keyboard layout? (tip: uk/us): " > KEYBOARD_LAYOUT
+read -p "Keyboard layout (uk/us) > " KEYBOARD_LAYOUT
 echo "Setting keyboard to: $KEYBOARD_LAYOUT"
 loadkeys $KEYBOARD_LAYOUT
 sleep 1
@@ -15,7 +15,7 @@ timedatectl status
 sleep 1
 
 # Partition the disks
-read -p "Swap partition size in G: " > SWAP_SIZE
+read -p "Swap partition size in G > " SWAP_SIZE
 echo ""$SWAP_SIZE"G swap"
 fdisk -l
 read -p "Type sdx > " DISK
