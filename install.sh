45ab06098bbe20d5982c6dda6fce0ecbe758851a
Roberto Rolo <robertorolo@gmail.com>
2020-06-29 20:12:36 -0300

    changed mirror

--- install.sh
+++ install.sh
@@ -107,7 +107,7 @@ else
 fi
 
 # Install essential packages
-echo -e "Server = http://archlinux.c3sl.ufpr.br/$repo/os/$arch\n$(cat /etc/pacman.d/mirrorlist)" > /etc/pacman.d/mirrorlist
+echo -e "Server = http://br.mirror.archlinux-br.org/$repo/os/$arch\n$(cat /etc/pacman.d/mirrorlist)" > /etc/pacman.d/mirrorlist
 echo "Instaling essential packages."
 pacstrap /mnt base linux linux-firmware git nano man-db man-pages texinfo networkmanager sudo curl
 
